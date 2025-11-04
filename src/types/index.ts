c5f2346b55d4ba35798f412823f13a8db8bdea8c
Aman Kumar Gupta <akgbytes@gmail.com>
2025-11-04 10:14:41 +0530

    add better-auth

---
+++ src/types/index.ts
@@ -0,0 +1,61 @@
+export interface Resume {
+  id: string;
+  companyName?: string;
+  jobTitle?: string;
+  imagePath: string;
+  resumePath: string;
+  feedback: Feedback;
+}
+
+export interface Feedback {
+  overallScore: number;
+  ATS: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+    }[];
+  };
+  toneAndStyle: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  content: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  structure: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  skills: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+}
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  image?: string | null | undefined;
+  emailVerified: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
